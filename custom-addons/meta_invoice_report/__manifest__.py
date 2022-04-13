d3b6285574ab9c1767e1a781b703fba9d1b39fd3
nsrshishir <nsrshishir550@gmail.com>
2022-04-13 11:46:05 +0600

    meta_invoice_report

---
+++ custom-addons/meta_invoice_report/__manifest__.py
@@ -0,0 +1,21 @@
+
+{
+    "name": "Invoice Report Customization",
+    "summary": "This Module change Invoice report ",
+    "version": "14.0.1.0.1",
+    "author": "Metamorphosis",
+    "website": "https://metamorphosis.com.bd",
+    "category": "Tools",
+    "depends": [
+        "base",
+        "account",
+    ],
+    "license": "LGPL-3",
+    "data": [
+        'views/report_invoice.xml',
+    ],
+    'sequence': 1,
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
